ba32a5d4ac1c69ebd7d214826ba573606d9a34f2
Daniel Munoz <146023389+dmunozbatista@users.noreply.github.com>
2025-04-26 18:40:43 -0500

    commented not used part

--- transit_feasible_areas/backend/inputs.py
+++ transit_feasible_areas/backend/inputs.py
@@ -5,30 +5,8 @@ import googlemaps
 # --- Setup ---
 app = FastAPI()
 
-def create_googlemaps_object():
-    return googlemaps.Client(key="xx")
-
-googlemap = create_googlemaps_object()
-
-class FeasibilityInput(BaseModel):
-    money_available: float
-    transportation_mode: str
-
-class AddressInput(BaseModel):
-    address: str
-
-
-# POST for feasible areas
-@app.post("/feasible_areas")
-def calculate_feasible_areas(input_data: FeasibilityInput):
-    return {
-        "message": f"You have ${input_data.money_available} available for {input_data.transportation_mode}."
-    }
-
-# POST for address geocoding
-@app.post("/geocode_address")
-def geocode_address(address_input: AddressInput):
-    geocode_result = googlemap.geocode(address_input.address)
+def geocode_address(address: str):
+    geocode_result = googlemaps.geocode(address)
 
     if not geocode_result:
         return {"error": "Address not found"}
@@ -39,8 +17,42 @@ def geocode_address(address_input: AddressInput):
     lng = location['lng']
 
     return {
-        "address": address_input.address,
+        "address": address,
         "latitude": lat,
         "longitude": lng
     }
+
+# class FeasibilityInput(BaseModel):
+#     money_available: float
+#     transportation_mode: str
+
+# class AddressInput(BaseModel):
+#     address: str
+
+
+# # POST for feasible areas
+# @app.post("/feasible_areas")
+# def calculate_feasible_areas(input_data: FeasibilityInput):
+#     return {
+#         "message": f"You have ${input_data.money_available} available for {input_data.transportation_mode}."
+#     }
+
+# # POST for address geocoding
+# @app.post("/geocode_address")
+# def geocode_address(address_input: AddressInput):
+#     geocode_result = googlemap.geocode(address_input.address)
+
+#     if not geocode_result:
+#         return {"error": "Address not found"}
+
+#     # Extract coordinates (lat, lng)
+#     location = geocode_result[0]['geometry']['location']
+#     lat = location['lat']
+#     lng = location['lng']
+
+#     return {
+#         "address": address_input.address,
+#         "latitude": lat,
+#         "longitude": lng
+#     }
 
