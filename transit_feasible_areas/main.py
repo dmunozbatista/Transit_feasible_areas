d3fe67489fc401e5e7bc21ca8fbb0f34bc32ddb1
Daniel Munoz <146023389+dmunozbatista@users.noreply.github.com>
2025-04-26 18:05:11 -0500

    added geo address

--- transit_feasible_areas/main.py
+++ transit_feasible_areas/main.py
@@ -5,11 +5,17 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import httpx
-
+import googlemaps
+from backend.inputs import geocode_address
 
 # App handler -----------------------------------------------
 
 app = FastAPI()
+
+def create_googlemaps_object():
+    return googlemaps.Client(key="xx")
+
+googlemap = create_googlemaps_object()
 
 # Resources -------------------------------------------------
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -30,7 +36,18 @@ async def login(
     money: Annotated[str, Form],
     address: Annotated[str, Form]
 ):
-    return {"time": time, "money":money, "address":address}
+    # Geocode the address
+    location_data = geocode_address(address)
+
+    if "error" in location_data:
+        return location_data  # You could later return a nicer error page
+
+    # For now, just return everything together
+    return {
+        "time": time,
+        "money": money,
+        "address_info": location_data
+    }
 
 
 # # EOF. ----------------------------------------------------
